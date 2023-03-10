67bc697fe14fa79d15ee552a76d1fcf6af05db05
sanketj0700 <sanket.jain@searce.com>
2023-03-10 20:17:13 +0530

    Functioning code. Tested on Google Cloud.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // module imports
 const gcipCloudFunctions = require('gcip-cloud-functions');
 const { getFirestore } = require("firebase-admin/firestore");
-const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const { initializeApp, applicationDefault, getApp, getApps } = require("firebase-admin/app");
 
 // Initializing Auth Client
 const authClient = new gcipCloudFunctions.Auth();
@@ -14,6 +14,14 @@ const initFirestore = () => {
 
   return db;
 };
+
+// initializing firestore client
+let firestore;
+if (!getApps().length) {
+  firestore = initFirestore();
+} else {
+  firestore = getApp();
+}
 
 // getDomainsForTenantUser - Fetches all the documents with given email frm tenantUser and returns a 
 const getDomainsForTenantUser = async (firestore, email) => {
@@ -32,11 +40,12 @@ const getDomainsForTenantUser = async (firestore, email) => {
 // beforeSignIn
 exports.beforeSignIn = authClient.functions().beforeSignInHandler(async (user, context) => {
   try {
-    // Initialize Firestore client
-    const firestore = initFirestore();
 
     // validDomains - domains that are allowed for the user email
+    console.log(user.email);
+
     const validDomains = await getDomainsForTenantUser(firestore, user.email);
+    console.log(validDomains);
 
     // setting custom claim on user
     return {
